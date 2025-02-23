0079af164a3221941ab5c0a0ac78502e5f009d79
unknown <reyesaldwin_bsit@plmun.edu.ph>
2025-02-22 18:04:11 -0700

    Submitting Python Week 04 - Laboratory # 16

---
+++ basic_function.py
@@ -0,0 +1,12 @@
+# ALDWIN REYES
+# ITELEC2
+# Laboratory #16– Guided Coding Exercise:
+# Variables, Literals, and Case-Sensitivity in Python (with Naming Conventions)
+
+def greet():
+    """Prints a greeting message."""
+    print("Hello, Python Students!")
+
+# Calling the function
+greet()
+
